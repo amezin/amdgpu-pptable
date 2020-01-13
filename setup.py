1ddf50a855945f49661749fae975dfdd47e08b63
Aleksandr Mezin <mezin.alexander@gmail.com>
2020-01-14 01:22:39 +0600

    Add pyproject.toml, get rid of setup.cfg

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setuptools.setup(
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     install_requires=['PyQt5'],
-    setup_requires=['pytest-runner', 'setuptools_scm', 'setuptools_scm_git_archive'],
-    tests_require=['pytest'],
+    setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
     entry_points={
         'console_scripts': ['amdgpu-pptable-to-json=amdgpu_pptable.dump:main'],
         'gui_scripts': ['amdgpu-pptable-editor=amdgpu_pptable.gui:main']
