ebedb3d7de5c3355293fb587419fa46507be529d
Aleksandr Mezin <mezin.alexander@gmail.com>
2020-03-15 15:37:58 +0600

    setup.py: remove console script
    This is a pure library now

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ setuptools.setup(
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
-    entry_points={
-        'console_scripts': ['amdgpu-pptable-to-json=amdgpu_pptable.dump:main']
-    },
     python_requires='>=3.6',
     use_scm_version={'write_to': 'src/amdgpu_pptable/version.py'}
 )
